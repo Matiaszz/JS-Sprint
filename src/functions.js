a7d855f6fe3d733efe1ca01384e29120d13bf373
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-11-20 18:09:04 -0300

    functions

---
+++ src/functions.js
@@ -0,0 +1,11 @@
+function sayHi(name) {
+  console.log(`sup ${name}!`);
+}
+sayHi("matias"); // -> sup matias!
+
+function multiply(n1, n2) {
+  const result = n1 * n2;
+  return result;
+}
+
+console.log(multiply(3, 2)); // -> 6
