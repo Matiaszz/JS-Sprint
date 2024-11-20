75777f57134deba17bf3368d07d242ace1ab4b65
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-11-20 17:51:36 -0300

    types

---
+++ types.js
@@ -0,0 +1,30 @@
+// Primitive types
+let favoriteIceCream = "Pinapple";
+let favoriteProgrammingLanguage = `Pyhton`;
+
+let intNumber = 12;
+let floatNumber = 1.2;
+let bigIntNumer = 8342732984732n;
+
+let iLoveCoding = true;
+
+let favoriteColor; // -> undefined
+let favoriteFruit = null;
+
+const uniqueKey = Symbol();
+
+let course = {
+  name: "js for begginers",
+  hours: 3,
+};
+
+// Dynamic types
+
+let firstName = "Matias";
+console.log(typeof firstName); // string
+
+firstName = 100;
+console.log(typeof firstName); // number
+
+firstName = false;
+console.log(typeof firstName); // boolean
