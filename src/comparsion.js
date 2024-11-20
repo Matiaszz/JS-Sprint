4d6e3b04da2157533199b4bd02193015c57cb588
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-11-20 18:17:04 -0300

    ternary operations and comparsion

---
+++ src/comparsion.js
@@ -0,0 +1,10 @@
+let n = 1;
+
+console.log(n == "1"); // true
+console.log(n === "1"); // false
+
+let points = 100;
+const game = points >= 100 ? "win" : "loose";
+// if condition be:
+//                            true    false
+console.log(game);
