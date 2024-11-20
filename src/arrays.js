635243e5357370859aa88ce55b5af9934b92ffb7
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-11-20 18:01:49 -0300

    arrays

---
+++ src/arrays.js
@@ -0,0 +1,6 @@
+let productColors = ["blue", "white", "black"];
+
+printf = console.log;
+printf(productColors); // prints all items of the array
+printf(productColors[1]); // referer by index to access the value and i can modify the value too
+printf(productColors.length); // its len() from python
