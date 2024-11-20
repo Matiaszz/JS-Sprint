ffe034bee6134f1bc3596d33318f9a731968d490
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-11-20 18:33:59 -0300

    if else and switch case

---
+++ switchCase.js
@@ -0,0 +1,33 @@
+let job = "Programmer";
+
+// instead if-else...
+
+//    if (job == "Programmer") {
+//      console.log("You're a programmer!");
+//  }    else if (job == "designer") {
+//      console.log("You're a designer!");
+//  }    else if (job == "Software engineer") {
+//      console.log("You're a software engineer!");
+//  } else {
+//      console.log("unemployed");
+//  }
+
+// use switch-case!
+switch (job) {
+  case "programmer":
+    console.log("You're a programmer!");
+    break;
+  case "programmer":
+    console.log("You're a programmer!");
+    break;
+  case "designer":
+    console.log("You're a designer!");
+    break;
+
+  case "software engineer":
+    console.log("You're a software engineer!");
+    break;
+
+  default:
+    console.log("unemployed.");
+}
