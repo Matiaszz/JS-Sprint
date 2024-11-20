90d40512e5b27e6a4ad80fe4f39023b90e88fc1a
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-11-20 18:38:07 -0300

    for

---
+++ src/forLoops.js
@@ -0,0 +1,10 @@
+let numbers = [1, 5, 4, 7, 99, 9, 4, 5];
+
+for (let i = 0; i < numbers.length; i++) {
+  const element = numbers[i];
+  console.log(element);
+}
+
+for (let i = 1; i < 31; i++) {
+  console.log(i);
+}
